af2cb21ede280ef3d91cdc7931b8c2d9b50bd136
Ray <top820510@gmail.com>
2020-12-30 16:55:20 +0800

    enter

---
+++ src/components/main/FunctionBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function FunctionBar() {
+  return (
+    <>
+      <div className="FunctionBar">
+        <a>我的帳戶</a>
+        <a>我的行程</a>
+        <a>我的揪團</a>
+        <a>我的收藏</a>
+        <a>我的通知</a>
+        <a>購物紀錄</a>
+      </div>
+    </>
+  )
+}
+
+export default FunctionBar
---
+++ src/pages/Member.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import Header from '../components/main/Header'
+import Footer from '../components/main/Footer'
+import StarRating from '../components/main/StarRating'
+import MemberList from '../components/main/MemberList'
+import CalendarApp from '../components/main/CalendarApp'
+import FunctionBar from '../components/main/FunctionBar'
+
+function Member() {
+  return (
+    <>
+      <Header />
+      <article>
+        <section>
+          <MemberList />
+          <StarRating />
+          <CalendarApp />
+        </section>
+        <nav>
+          <FunctionBar />
+        </nav>
+      </article>
+      <Footer />
+    </>
+  )
+}
+
+export default Member

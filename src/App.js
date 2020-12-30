220ccc90f42ebab9e84385c79dace7df36fe2654
Ray <top820510@gmail.com>
2020-12-30 16:40:45 +0800

    語意化標籤

--- src/App.js
+++ src/App.js
@@ -4,14 +4,22 @@ import Footer from './components/main/Footer'
 import StarRating from './components/main/StarRating'
 import MemberList from './components/main/MemberList'
 import CalendarApp from './components/main/CalendarApp'
+import FunctionBar from './components/main/FunctionBar'
 
 function App() {
   return (
     <>
       <Header />
-      <MemberList />
-      <StarRating />
-      <CalendarApp />
+      <article>
+        <section>
+          <MemberList />
+          <StarRating />
+          <CalendarApp />
+        </section>
+        <nav>
+          <FunctionBar />
+        </nav>
+      </article>
       <Footer />
     </>
   )

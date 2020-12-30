9091f785bce7ec3bf76de0eb29a566697a9e5ade
kkshen3352 <h73423352@gmail.com>
2020-12-30 17:16:50 +0800

    新增卡片的圖(絕對路徑)

--- src/components/MeFavorites.js
+++ src/components/MeFavorites.js
@@ -15,15 +15,18 @@ function MeFavorites() {
         <div className="row no-gutters">
           <div className="col-md-4">
             <img
-              src="..."
-              className="card-img"
+              // 要放絕對路徑
+              src="http://localhost:3000/images/DSC_7437-37.jpg"
+              className="card-img img-fluid"
               alt="..."
               style={cardimgstyle}
             />
           </div>
-          <div className="col-md-8">
+          <div className="col-md-8 align-items-end">
             <div className="card-body">
               <h3 className="card-title">台南蔥油餅吃到飽之旅台南蔥油餅</h3>
+              <br />
+              <br />
               <p className="card-text">
                 <small className="text-muted">icon&svg</small>
               </p>

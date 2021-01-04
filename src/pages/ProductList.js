0c8c5ad15df70437ce7acf54c8c2631b508be014
Ray <top820510@gmail.com>
2021-01-04 16:46:24 +0800

    存一下 Ray

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -4,6 +4,8 @@ import MyFooter from '../components/main/MyFooter'
 import MyBreadCrumb from '../components/main/MyBreadCrumb'
 import SearchBar from '../components/main/SearchBar'
 import CardList from '../components/products/CardList'
+import '../style/productList.scss'
+import { Col } from 'react-bootstrap'
 
 function ProductList() {
   return (
@@ -12,7 +14,9 @@ function ProductList() {
       <div className="container">
         <MyBreadCrumb />
         <h1>達人講座</h1>
-        <SearchBar />
+        <Col md={8}>
+          <SearchBar />
+        </Col>
         <CardList />
       </div>
       <MyFooter />

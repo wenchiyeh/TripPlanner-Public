9b058c40000829f7052e4739db30c3eaecbe38ea
Ray <top820510@gmail.com>
2021-01-04 14:02:38 +0800

    卡片好像有點巨 Ray

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -3,6 +3,8 @@ import Header from '../components/main/Header'
 import MyFooter from '../components/main/MyFooter'
 import MyBreadCrumb from '../components/main/MyBreadCrumb'
 import ProductSlider from '../components/products/ProductSlider'
+import SearchBar from '../components/main/SearchBar'
+import CardList from '../components/products/CardList'
 
 function ProductList() {
   return (
@@ -10,7 +12,8 @@ function ProductList() {
       <Header />
       <MyBreadCrumb />
       <ProductSlider />
-
+      <SearchBar />
+      <CardList />
       <MyFooter />
     </>
   )

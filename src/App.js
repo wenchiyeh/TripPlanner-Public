a11f920886a8da6e5864979b7f2a153c533d4b29
qq6178qq <qq6178qq@gmail.com>
2021-01-05 16:31:09 +0800

    home upper

--- src/App.js
+++ src/App.js
@@ -10,36 +10,15 @@ import Member from './pages/Member'
 import SearchBar from './components/main/SearchBar'
 import Footer from './components/main/Footer'
 import Header from './components/main/Header'
-
-
+import Home from './pages/Home'
 
 function App() {
   return (
     <Router>
       <>
-        <Link to="/">Home</Link>
-        <Link to="/member">Member</Link>
-        <Link to="/itinerary">Itinerary</Link>
-        <Link to="/group">Group</Link>
-        <Link to="/cash">Cash</Link>
-        <Link to="/product">Product</Link>
-        <Link to="/search">SearchBar</Link>
-
-        <Switch>
-          <Route path="/member">
-            <Member />
-          </Route>
-          <Route path="/itinerary">
-            <Itinerary />
-          </Route>
-          <Route path="/search">
-            <SearchBar />
-          </Route>
-          <Route path="/">
-            <h1>Hello World</h1>
-          </Route>
-        </Switch>
+        <Home />
         <Header />
+
         <Footer />
       </>
     </Router>

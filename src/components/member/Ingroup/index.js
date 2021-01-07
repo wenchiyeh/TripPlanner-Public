ba09a655908808a2efd3d330c4095485a866f443
kkshen <h73423352@gmail.com>
2021-01-07 20:42:59 +0800

    正在參與的揪團

--- src/components/member/Ingroup/index.js
+++ src/components/member/Ingroup/index.js
@@ -9,52 +9,54 @@ let handleTestData = cardData[2].data
 //測試資料可以做成JSON檔之後用這個方式引入
 
 function MeFavoritesgroup({
-  title = '揪團蔥油餅吃到飽之旅台灣蔥油餅', //標題
-  image, //圖片的檔名與附檔名ex: 'testImage.jpg'
-  time1 = '2020/01/01', //第一個日期
-  time2 = '2020/01/01', //第二個日期
+  data = handleTestData,
+  type = 'itinerary',
+  title = '台北一日到處遊',
   map1 = '北部', //地區1
   map2 = '桃園', //地區2
-  duration = '3', //天數
-  person = '小智', //卡片內顯示的人名
 }) {
-  return (
-    <>
-      <div className="card mb-3">
-        <div className="row no-gutters me-favorites-back-style">
-          <div className="col-md-4">
-            <img
-              // 要放絕對路徑
-              src="http://localhost:3000/images/member/DSC_7437-37.jpg"
-              className="card-img img-fluid"
-              alt="..."
-            />
-          </div>
-          <div className="col-md-8 align-items-end">
-            <div className="card-body">
-              <h3 className="card-title">{title}</h3>
-              <span className="mef-icno-style">
-                <IoMdTime />
-                {time1} - {time2}
-              </span>
-              <span className="mef-icno-style d-flex justify-content-between">
-                {/* 地圖位置1 */}
-                <p className="card-style-mef ">
-                  <FaMapMarkerAlt />
-                  {map1}
-                </p>
-                <p className="card-style-mef mef-icon-map-move">
-                  <FaMapMarkerAlt />
-                  {map2}
-                </p>
-                <button className="Ingroup-btn">聊天室</button>
-              </span>
-              <br />
+  let display = <></>
+  if (type === 'itinerary') {
+    if (title === '台北一日到處遊') {
+      display = data.map((element, index) => (
+        <div key={index} className="card mb-3">
+          <div className="row no-gutters me-favorites-back-style">
+            <div className="col-md-4">
+              <img
+                // 要放絕對路徑
+                src="http://localhost:3000/images/member/DSC_7437-37.jpg"
+                className="card-img img-fluid"
+                alt="..."
+              />
+            </div>
+            <div className="col-md-8 align-items-end">
+              <div className="card-body">
+                <h3 className="card-title">{element.title}</h3>
+                <span className="mef-icno-style">
+                  <IoMdTime />
+                  {element.establish_time} - {element.publish_time}
+                </span>
+                <span className="mef-icno-style d-flex justify-content-between">
+                  {/* 地圖位置1 */}
+                  <p className="card-style-mef ">
+                    <FaMapMarkerAlt />
+                    {map1}
+                  </p>
+                  <p className="card-style-mef mef-icon-map-move">
+                    <FaMapMarkerAlt />
+                    {map2}
+                  </p>
+                  <button className="Ingroup-btn">聊天室</button>
+                </span>
+                <br />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </>
-  )
+      ))
+    }
+  }
+
+  return <>{display}</>
 }
 export default MeFavoritesgroup

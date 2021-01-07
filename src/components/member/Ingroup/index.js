62becf9b60a04fed4d9b32262309c00f153eab55
kkshen <h73423352@gmail.com>
2021-01-07 20:56:59 +0800

    修正我的揪團收藏卡片

--- src/components/member/Ingroup/index.js
+++ src/components/member/Ingroup/index.js
@@ -17,44 +17,42 @@ function MeFavoritesgroup({
 }) {
   let display = <></>
   if (type === 'itinerary') {
-    if (title === '台北一日到處遊') {
-      display = data.map((element, index) => (
-        <div key={index} className="card mb-3">
-          <div className="row no-gutters me-favorites-back-style">
-            <div className="col-md-4">
-              <img
-                // 要放絕對路徑
-                src="http://localhost:3000/images/member/DSC_7437-37.jpg"
-                className="card-img img-fluid"
-                alt="..."
-              />
-            </div>
-            <div className="col-md-8 align-items-end">
-              <div className="card-body">
-                <h3 className="card-title">{element.title}</h3>
-                <span className="mef-icno-style">
-                  <IoMdTime />
-                  {element.establish_time} - {element.publish_time}
-                </span>
-                <span className="mef-icno-style d-flex justify-content-between">
-                  {/* 地圖位置1 */}
-                  <p className="card-style-mef ">
-                    <FaMapMarkerAlt />
-                    {map1}
-                  </p>
-                  <p className="card-style-mef mef-icon-map-move">
-                    <FaMapMarkerAlt />
-                    {map2}
-                  </p>
-                  <button className="Ingroup-btn">聊天室</button>
-                </span>
-                <br />
-              </div>
+    display = data.map((element, index) => (
+      <div key={index} className="card mb-3">
+        <div className="row no-gutters me-favorites-back-style">
+          <div className="col-md-4">
+            <img
+              // 要放絕對路徑
+              src="http://localhost:3000/images/member/DSC_7437-37.jpg"
+              className="card-img img-fluid"
+              alt="..."
+            />
+          </div>
+          <div className="col-md-8 align-items-end">
+            <div className="card-body">
+              <h3 className="card-title">{element.title}</h3>
+              <span className="mef-icno-style">
+                <IoMdTime />
+                {element.establish_time} - {element.publish_time}
+              </span>
+              <span className="mef-icno-style d-flex justify-content-between">
+                {/* 地圖位置1 */}
+                <p className="card-style-mef ">
+                  <FaMapMarkerAlt />
+                  {map1}
+                </p>
+                <p className="card-style-mef mef-icon-map-move">
+                  <FaMapMarkerAlt />
+                  {map2}
+                </p>
+                <button className="Ingroup-btn">聊天室</button>
+              </span>
+              <br />
             </div>
           </div>
         </div>
-      ))
-    }
+      </div>
+    ))
   }
 
   return <>{display}</>

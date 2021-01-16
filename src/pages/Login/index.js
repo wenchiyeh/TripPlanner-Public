e9a65aaf119da8ae7551092c25e8e400d30de79e
kkshen <h73423352@gmail.com>
2021-01-17 00:36:05 +0800

    登入驗證 測試

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -5,7 +5,8 @@ import { Form, Button, Col, InputGroup } from 'react-bootstrap'
 import './login.scss'
 import { useHistory, Link } from 'react-router-dom'
 
-function Login({ onLogin }) {
+function Login(props) {
+  const { isAuth, setIsAuth } = props
   //const [loading, setLoading] = useState(false)
   //const [timer, setTimer] = useState(null)
   //
@@ -35,13 +36,15 @@ function Login({ onLogin }) {
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
       })
+      console.log(email)
       if (response.ok) {
         const data = await response.json()
         if (data.result) {
           setMember(data.member)
           history.push('/myAccount')
         } else {
-          history.push('/login')
+          console.log('請輸入正確的帳號密碼')
+          //history.push('/login')
         }
       }
     } catch (err) {
@@ -66,120 +69,129 @@ function Login({ onLogin }) {
   // }, [email, password])
   //要寫useEffect
   useEffect(() => {
-    if (member > -1) {
+    if (member > 0) {
       console.log(`登入成功 會員: ${member}`)
-      history.push('/myAccount')
+      setMember()
     } else {
-      //console.log('請重新輸入')
-      history.push('/login')
+      console.log('請重新輸入')
+      return (
+        <>
+          <Form.Control.Feedback type="invalid">
+            請輸入正確的密碼格式
+          </Form.Control.Feedback>
+        </>
+      )
+      //history.push('/login')
     }
   }, [member])
-  return (
-    <>
-      <body className="body-login">
-        <div className="login-form form-group">
-          <h1>會員帳號登入</h1>
-          <Form noValidate validated={validated} onSubmit={handleSubmit}>
-            <Form.Row>
-              <Form.Group as={Col} md="10" controlId="validationCustomUsername">
-                <InputGroup>
-                  <InputGroup.Prepend>
-                    <InputGroup.Text id="inputGroupPrepend">
-                      <FaUserAlt />
-                    </InputGroup.Text>
-                  </InputGroup.Prepend>
-                  <Form.Control
-                    type="text"
-                    placeholder="您的信箱"
-                    aria-describedby="inputGroupPrepend"
-                    required
-                    onChange={(e) => {
-                      setEmail(e.target.value)
-                    }}
-                  />
-                  <Form.Control.Feedback type="invalid">
-                    請輸入正確的信箱格式
-                  </Form.Control.Feedback>
-                </InputGroup>
-              </Form.Group>
-            </Form.Row>
-            <Form.Row>
-              <Form.Group as={Col} md="10" controlId="validationCustomUsername">
-                <InputGroup>
-                  <InputGroup.Prepend>
-                    <InputGroup.Text id="inputGroupPrepend">
-                      <FaUnlockAlt />
-                    </InputGroup.Text>
-                  </InputGroup.Prepend>
-                  <Form.Control
-                    className="login-input-br"
-                    type="text"
-                    placeholder="您的密碼"
-                    aria-describedby="inputGroupPrepend"
-                    required
-                    onChange={(e) => {
-                      setPassword(e.target.value)
-                    }}
-                  />
-                  <Form.Control.Feedback type="invalid">
-                    請輸入正確的密碼格式
-                  </Form.Control.Feedback>
-                </InputGroup>
-              </Form.Group>
-            </Form.Row>
-            <Button
-              type="submit"
-              className="login-btn"
-              // state={loading ? 'loading' : undefined}
-              // onClick={() => {
-              //   setLoading(true)
-              //   setTimer(
-              //     setTimeout(
-              //       () => (onLogin ? onLogin() : setLoading(false)),
-              //       2000
-              //     )
-              //   )
-              // }}
-            >
-              登入
-            </Button>
-            <div className="login-samp-text d-flex">
-              <span>
-                <Link
-                  onClick={() => {
-                    history.push('/sigon')
+
+  const display = (
+    <div className="body-login">
+      <div className="login-form form-group">
+        <h1>會員帳號登入</h1>
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+          <Form.Row>
+            <Form.Group as={Col} md="10" controlId="validationCustomUsername">
+              <InputGroup>
+                <InputGroup.Prepend>
+                  <InputGroup.Text id="inputGroupPrepend">
+                    <FaUserAlt />
+                  </InputGroup.Text>
+                </InputGroup.Prepend>
+                <Form.Control
+                  type="text"
+                  placeholder="您的信箱"
+                  aria-describedby="inputGroupPrepend"
+                  required
+                  onChange={(e) => {
+                    setEmail(e.target.value)
                   }}
-                >
-                  註冊
-                </Link>
-              </span>
-              <span className="login-samp-text-pas">
-                <Link
-                  onClick={() => {
-                    history.push('/forgetpassword')
+                />
+                <Form.Control.Feedback type="invalid">
+                  請輸入正確的信箱格式
+                </Form.Control.Feedback>
+              </InputGroup>
+            </Form.Group>
+          </Form.Row>
+          <Form.Row>
+            <Form.Group as={Col} md="10" controlId="validationCustomUsername">
+              <InputGroup>
+                <InputGroup.Prepend>
+                  <InputGroup.Text id="inputGroupPrepend">
+                    <FaUnlockAlt />
+                  </InputGroup.Text>
+                </InputGroup.Prepend>
+                <Form.Control
+                  className="login-input-br"
+                  type="text"
+                  placeholder="您的密碼"
+                  aria-describedby="inputGroupPrepend"
+                  required
+                  onChange={(e) => {
+                    setPassword(e.target.value)
                   }}
-                >
-                  忘記密碼
-                </Link>
-              </span>
-            </div>
-            <div className="d-flex login-line-center">
-              <span className="login-line"></span>
-              <h1>OR</h1>
-              <span className="login-line"></span>
-            </div>
-            <div className="login-icon-fa d-flex">
-              <span>
-                <FaFacebook />
-              </span>
-              <span>
-                <FaGoogle />
-              </span>
-            </div>
-          </Form>
-        </div>
-      </body>
-    </>
+                />
+                <Form.Control.Feedback type="invalid">
+                  請輸入正確的密碼格式
+                </Form.Control.Feedback>
+              </InputGroup>
+            </Form.Group>
+          </Form.Row>
+          <Button
+            type="submit"
+            className="login-btn"
+            // state={loading ? 'loading' : undefined}
+            // onClick={() => {
+            //   setLoading(true)
+            //   setTimer(
+            //     setTimeout(
+            //       () => (onLogin ? onLogin() : setLoading(false)),
+            //       2000
+            //     )
+            //   )
+            // }}
+          >
+            登入
+          </Button>
+          <div className="login-samp-text d-flex">
+            <span>
+              <Link
+                to="/sigon"
+                onClick={() => {
+                  history.push('/sigon')
+                }}
+              >
+                註冊
+              </Link>
+            </span>
+            <span className="login-samp-text-pas">
+              <Link
+                to="forgetpassword"
+                onClick={() => {
+                  history.push('/forgetpassword')
+                }}
+              >
+                忘記密碼
+              </Link>
+            </span>
+          </div>
+          <div className="d-flex login-line-center">
+            <span className="login-line"></span>
+            <h1>OR</h1>
+            <span className="login-line"></span>
+          </div>
+          <div className="login-icon-fa d-flex">
+            <span>
+              <FaFacebook />
+            </span>
+            <span>
+              <FaGoogle />
+            </span>
+          </div>
+        </Form>
+      </div>
+    </div>
   )
+  return display
 }
 export default Login

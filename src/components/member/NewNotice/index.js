d8d89da47e7cb4591cf7ea53eaf3bddafa24c1e7
starknows <xing255161@gmail.com>
2021-01-19 12:04:16 +0800

    解衝突3 wuxu

--- src/components/member/NewNotice/index.js
+++ src/components/member/NewNotice/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //通知表單
 import React from 'react'
 import './newnotice.scss'
@@ -36,42 +35,3 @@ function NewNotice({
 }
 
 export default NewNotice
-=======
-//通知表單
-import React from 'react'
-import './newnotice.scss'
-
-function NewNotice({
-  id = '1',
-  data = '24週年慶站長大放送 - 抽獎 Call Out 回答通關密語',
-  time = '兩小時前',
-}) {
-  return (
-    <>
-      <div className="">
-        <table className="not-toble-ma-bt table-hover">
-          <tr>
-            {/* <th scope="col">No.</th> */}
-            <th>旅行日期</th>
-            {/* <th className="text-right" scope="col">
-              時間
-            </th> */}
-          </tr>
-          <tbody>
-            <tr className="d-flex new-not-flex">
-              <td className="not-td-left">{data}</td>
-              <td className="not-td-right">{time}</td>
-            </tr>
-            <tr className="d-flex new-not-flex">
-              <td className="not-td-left">{data}</td>
-              <td className="not-td-right">{time}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </>
-  )
-}
-
-export default NewNotice
->>>>>>> a100240a069a90278c78fb8cb4132985577c7b55
--- src/pages/Notice/index.js
+++ src/pages/Notice/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //通知表單
 import React from 'react'
 import Pages from '../../components/main/Pages'
@@ -47,53 +46,3 @@ function Notice({
 }
 
 export default Notice
-=======
-//通知表單
-import React from 'react'
-import Pages from '../../components/main/Pages'
-import { Table } from 'react-bootstrap'
-import './notice.scss'
-
-let cardData = require('../../components/Itinerary/testJsonData.json')
-let handleTestData = cardData[2].data
-
-function Notice({
-  data = handleTestData,
-  type = 'itinerary',
-  //id = '1',
-  title = '24週年慶站長大放送 - 抽獎 Call Out 回答通關密語',
-  time = '兩小時前',
-}) {
-  let display = <></>
-  if (type === 'itinerary') {
-    display = data.map((element, index) => (
-      <tbody>
-        <tr key={index}>
-          <td>{element.id}</td>
-          <td className="not-td-left">{element.title}</td>
-          <td className="not-td-right">{time}</td>
-        </tr>
-      </tbody>
-    ))
-  }
-  return (
-    <>
-      <Table className="table table-striped">
-        <div className="notice-style">
-          <thead>
-            <tr className="not-table-mrove">
-              <th className="not-id">No.</th>
-              <th className="text-left">旅行日期</th>
-              <th className="text-right-time">時間</th>
-            </tr>
-          </thead>
-          {display}
-          <Pages className="not-pages" />
-        </div>
-      </Table>
-    </>
-  )
-}
-
-export default Notice
->>>>>>> a100240a069a90278c78fb8cb4132985577c7b55
